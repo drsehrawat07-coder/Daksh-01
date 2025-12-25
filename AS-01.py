2ec5a469abfca8ac3b1770eeb38c834abf58538c
Daksh Sehrawat <drsehrawat07@gmail.com>
2025-12-25 20:00:57 +0530

    Add

---
+++ AS-01.py
@@ -0,0 +1,17 @@
+# Simple calculator for only two numbers!
+
+# Taking input from the user:-
+a = float(input("Enter the 1st Number :- "))
+b = float(input("Enter the 2st Number :- "))
+
+# Calculations:- 
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+
+# Output:-
+print("/nAddition : ", addition)
+print("Subtraction : ", subtraction)
+print("Multiplication : ", multiplication)
+print("Division : ", division)
---
+++ AS-02.py
@@ -0,0 +1,4 @@
+a = input("Enter your First name : ")
+b = input("Enter your Second name : ")
+
+print(f"Hello, {a} {b}! Welcome to the Python Program")
